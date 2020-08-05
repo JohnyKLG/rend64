0124b5c4eb1b56f8c86bfb3d6283eab1caf68688
Jonathan Vieira <johnyklg@outlook.com>
2020-08-05 10:19:50 -0300

    dev rend64.js

--- rend64.js
+++ rend64.js
@@ -1,5 +1,6 @@
-window.onload = function(){bd = document.body;
-    var dstl = "position:fixed;top:9px;width:250px;height:250px;border:1px solid #000;";
+window.onload = function(){
+    bd = document.body;bd.style = 'background:#ccc;color:#666;'
+    var dstl = "position:fixed;top:9px;width:250px;height:250px;background:#666;border:1px solid #000;";
     var din = document.createElement('div');din.style = dstl+'left:9px;';
     din.id = 'din';bd.appendChild(din);
     var dout = document.createElement('div');dout.style = dstl+'left:399px;';
@@ -10,21 +11,32 @@ window.onload = function(){bd = document.body;
     var ostl = "position:fixed;top:10px;left:400px;max-width:250px;max-height:250px;";
     var cout = document.createElement('img');cout.style = ostl;
     cout.id = 'iout';bd.appendChild(cout);
-    var fstl = "position:fixed;top:270px;left:9px;";
+    var fstl = "position:fixed;top:270px;left:9px;width:115px;";
     var cfle = document.createElement('input');cfle.style = fstl;
     cfle.type = 'file';cfle.accept = 'image/*';cfle.id = 'fle';
     cfle.setAttribute('onchange','imgIn(event)');
     bd.appendChild(cfle);
-    var wstl = "position:fixed;top:300px;left:9px;";
+    var wstl = "position:fixed;top:270px;left:147px;width:60px;";
     var cwdt = document.createElement('input');cwdt.style = wstl;
     cwdt.type = 'number';cwdt.id = 'cwdt';bd.appendChild(cwdt);
-    var hstl = "position:fixed;top:330px;left:9px;";
+    var hstl = "position:fixed;top:270px;left:260px;width:60px;";
     var chgt = document.createElement('input');chgt.style = hstl;
     chgt.type = 'number';chgt.id = 'chgt';bd.appendChild(chgt);
-    var tstl = "position:fixed;top:360px;left:9px;width:636px;height:200px;";
+    var swstl = "position:fixed;top:272px;left:135px;font:15px sans-serif;";
+    var swdt = document.createElement('span');swdt.innerText = 'L';
+    swdt.style = swstl;bd.appendChild(swdt);
+    var shstl = "position:fixed;top:272px;left:247px;font:15px sans-serif;";
+    var shgt = document.createElement('span');shgt.innerText = 'A';
+    shgt.style = shstl;bd.appendChild(shgt);
+    var swstl = "position:fixed;top:270px;left:217px;font:15px sans-serif;";
+    var swdt = document.createElement('span');swdt.innerText = 'px';
+    swdt.style = swstl;bd.appendChild(swdt);
+    var shstl = "position:fixed;top:270px;left:330px;font:15px sans-serif;";
+    var shgt = document.createElement('span');shgt.innerText = 'px';
+    shgt.style = shstl;bd.appendChild(shgt);
+    var tstl = "position:fixed;top:300px;left:9px;width:636px;height:200px;resize: none;";
     var ctxt = document.createElement('textarea');ctxt.style = tstl;
-    ctxt.id = 'ctxt';
-    bd.appendChild(ctxt);
+    ctxt.id = 'ctxt';bd.appendChild(ctxt);
 }
 function canvas64(img){
     var canvas = document.createElement("canvas");
@@ -39,17 +51,17 @@ function canvas64(img){
 }
 function png64(){
     var texto = canvas64(document.getElementById('iin'));
-    var dist = 72;
+    var dist = 76;
     var resultado = new Array(parseInt(texto.length / dist));
     for(var x = 0; x < texto.length / dist; x++){
-        document.getElementById('ctxt').value+= "cnfg64+= '"+texto.substring(0 + x * dist, (x + 1) * dist)+"';\n";
+        document.getElementById('ctxt').value+= "vOut '"+texto.substring(0 + x * dist, (x + 1) * dist)+"';\n";
     }
     var iout = document.getElementById('iout');
     iout.src = texto;
     document.getElementById('btn').remove();
     var bstl = "position:fixed;top:270px;left:9px;";
     var cbtn = document.createElement('button');cbtn.style = bstl;
-    cbtn.innerText = 'Novo';cbtn.id = 'btn';
+    cbtn.innerText = 'Novo arquivo';cbtn.id = 'btn';
     cbtn.setAttribute('onclick','location.reload()');bd.appendChild(cbtn);
 }
 var imgIn = function(event){
@@ -61,7 +73,7 @@ var imgIn = function(event){
         document.getElementById('fle').remove();
         var bstl = "position:fixed;top:270px;left:9px;";
         var cbtn = document.createElement('button');cbtn.style = bstl;
-        cbtn.innerText = 'Gerar';cbtn.id = 'btn';
+        cbtn.innerText = 'Renderizar';cbtn.id = 'btn';
         cbtn.setAttribute('onclick','png64()');bd.appendChild(cbtn);
         iin.style.maxWidth = '250px';
         iin.style.maxHeight = '250px';
